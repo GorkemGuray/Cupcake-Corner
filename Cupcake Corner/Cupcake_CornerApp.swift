90a135c60f230c0334eb17bfd09e2f85f246a4a8
Görkem Güray <grkmgry@gmail.com>
2024-06-24 22:38:43 +0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Görkem Güray on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
