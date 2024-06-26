fd29a432238607a095275cfc2aff710bd763fbf4
Görkem Güray <grkmgry@gmail.com>
2024-06-26 22:54:52 +0300

    Day-49 #Validating and disabling forms

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -7,76 +7,31 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable{
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
     
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
+    var disableForm : Bool {
+        username.count < 5 || email.count < 5
+    }
     
     var body: some View {
-        
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) {image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            ProgressView()
-        }
-        .frame(width: 200, height: 200)
-        
-        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) {phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
-            }
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            } //Section - 1
             
-        }
-        .frame(width: 200, height: 200)
-
-        
-        List(results, id: \.trackId) { item in
-            VStack (alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            } //VStack
-            
-        } //List
-        .task {
-            await loadData()
-        }
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                } //Button
+            } //Section - 2
+            .disabled(disableForm)
+        } //Form
     } //body
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        } //guard let url
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        } //do-catch
-        
-    } //func loadData
     
 } //ContentView
 
