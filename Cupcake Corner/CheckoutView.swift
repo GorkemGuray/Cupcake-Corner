c86ba23125d6a0803300ec4a987818b1bc0276dd
Görkem Güray <grkmgry@gmail.com>
2024-07-02 23:01:31 +0300

    Day-51 #Checking for a valid address

---
+++ Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Görkem Güray on 2.07.2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
